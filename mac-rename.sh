35a087dc04f955f5a13f43d301ad483ebf6ea095
Ryan Shaver <32579646+ACloseShave@users.noreply.github.com>
2020-10-17 17:52:20 -0500

    Comment update
    Last 6, not last 7 comment (stated last 7)

--- mac-rename.sh
+++ mac-rename.sh
@@ -4,7 +4,7 @@
 
 serial=$(ioreg -rd1 -c IOPlatformExpertDevice | awk -F'"' '/IOPlatformSerialNumber/{print $4}' | tail -c 7)
 
-#Change computer names to "OSX-" + last 7 
+#Change computer names to "OSX-" + last 6 
 
 /usr/sbin/scutil --set ComputerName "OSX-$serial"
 /usr/sbin/scutil --set LocalHostName "OSX-$serial"
